8cb41376e1821df80a378baef9bbccb0e167ebdb
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2023-12-23 23:55:21 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className=" bg-black mx-auto my-0 flex justify-center max-w-[390px]">
+      hello
+    </div>
+  );
+}
+
+export default App;
