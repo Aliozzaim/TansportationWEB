4b2c0f79c700ba5a227241b843a0918d1e14b92a
Tayfun Açıcı <tyfnacici@gmail.com>
2024-01-11 14:52:45 +0300

    fix some stuff

---
+++ app/new-offer/page.jsx
@@ -0,0 +1,49 @@
+"use client"
+import React, { useState } from "react"
+import { redirect } from "next/navigation"
+import TextInput from "../components/atoms/TextInput"
+
+const CreateProfilePage = () => {
+  return (
+    <>
+      <div className="card !w-96 p-9 rounded-[36px] bg-[#1A1A1A] justify-items-center text-center mt-[52px] mb-[136px] mx-auto">
+        <h3 className="font-[400] text-[16px] text-[#FF6438] pb-[10px]">
+          Hoş geldiniz
+        </h3>
+        <h3 className="dmsans70024">Profil Oluştur</h3>
+
+        <div className=" relative file-upload !w-24 !h-24 rounded-full   bg-[#222222] mx-auto mt-9 mb-6">
+          <input
+            // ref={fileInput}
+            type="file"
+            accept="image/*"
+            name="file"
+            // onChange={uploadHandler}
+            className=" bg-[#222222] "
+            style={{ display: "none" }}
+          />
+          <label
+            htmlFor="file"
+            className="custom-file-input  absolute top-1/2 right-1/2 transform translate-x-1/2 -translate-y-1/2"
+          ></label>
+          <button
+            className="btn_primary !w-[4.375rem] !h-8 !rounded-full dmsans50014  absolute bottom-0 right-1/2 transform translate-x-1/2 translate-y-1/2 z-10"
+            // onClick={() => fileInput.current.click()}
+          >
+            Ekle
+          </button>
+        </div>
+        <div className="!max-w-[18.75rem] mt-6">
+          <button
+            // onClick={onclickHandler}
+            className="btn_primary !max-w-[18.75rem] !h-[50px] rounded-2xl dmsans50016 mt-6"
+          >
+            Tamamla
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default CreateProfilePage
---
+++ app/offer-detail/layout.jsx
@@ -0,0 +1,58 @@
+"use client";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import backBtnIcon from "../../public/assets/images/backArrow-right.svg";
+import FooterPartial from "../components/FooterPartial";
+
+export default function RootLayout({ children }) {
+  const router = useRouter();
+
+  const handleBackClick = () => {
+    router.back();
+  };
+
+  return (
+    <>
+      <div className="flex flex-row">
+        <div className="pt-16 !w-full relative">
+          <button
+            className=" items-center flex  fixed justify-center  border border-solid border-gray-700 w-[42px] h-[42px] rounded-full bg-[#343434] "
+            onClick={handleBackClick}
+          >
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g opacity="0.5">
+                <path
+                  d="M7.9751 15.0583L2.91676 9.99999L7.9751 4.94165"
+                  stroke="white"
+                  strokeWidth="1.5"
+                  strokeMiterlimit="10"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M17.0833 10L3.05825 10"
+                  stroke="white"
+                  strokeWidth="1.5"
+                  strokeMiterlimit="10"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </g>
+            </svg>
+          </button>
+
+          {children}
+        </div>
+      </div>
+      <footer className="border-t container dmsans mt-5 border-solid border-[#2C2C2C] py-[36px] rounded-b-2xl flex justify-start pl-[2rem] gap-[9rem] text-[16px] font-[400] leading-[21px] footer_t1 mb-10">
+        <FooterPartial />
+      </footer>
+    </>
+  )
+}
